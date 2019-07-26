18392849ab669ffde2fc3e52b3bbc15cdbf08f08
danielm1015 <daniel.martinez1015@gmail.com>
2019-07-26 15:29:03 -0700

    update

--- steps/connectapi.py
+++ steps/connectapi.py
@@ -1,10 +1,25 @@
-class ConnectAPI:
-    
-    @property
-    def api_connect(self):
-        print("In api_connect(self)")
-        return self._response
+from behave import *
+import requests
 
-    @api_connect.setter
-    def api_connect(self, context):
-        print("In api_connect(self, context)")
+class ConnectAPI():
+    
+    def __init__(self, response):
+        self._data = data
+
+    @property
+    def connectAPI(self):
+        print("Getting Value")
+        return self._data
+
+    @connectAPI.setter
+    def connectAPI(self, context):
+        print("Setting Value")
+
+        URL = context
+        API_USER = "user"
+        API_PASSWD = "password"
+
+        user_pass = (API_USER, API_PASSWD)
+        response = requests.get(URL, auth=user_pass, verify=False)
+        
+        self._data = response.json()
--- steps/test.py
+++ steps/test.py
@@ -2,9 +2,15 @@ from behave import *
 import requests
 from connectapi import ConnectAPI
 
+
 @given('the Continuous Automated Build System exists')
 def step_impl(context):
-    print("context string: "+ context.text)
+    print("This is the context string: "+ context.text)
+   
+    data = ConnectAPI(context.text)
+    print(" Setting variable data with request connection: ", data)
+    print(" Calling Getter to : ", data.connectAPI)
+    
 
 @when('I query the Ansible API for the CABS Operating System')
 def step_impl(context):
